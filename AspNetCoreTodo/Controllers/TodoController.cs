6e20752b56fe410462b22e6069c4258b41543bc0
Thibault LEGRAND <thibault.legrand@axess.fr>
2019-06-25 15:25:59 +0200

    commit xxx

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        private ITodoItemService _todoItemService;
+        public async Task<IActionResult> IndexAsync()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            return View();
+        }
+    }
+}
